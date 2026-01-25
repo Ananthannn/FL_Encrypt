08100f715199cdeed9eef6e9bf760d79cc036993
hue9750 <rachelsara2005@gmail.com>
2026-01-25 21:04:47 +0530

    added numpy to client.py

--- client/client.py
+++ client/client.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import tensorflow as tf
+import numpy as np 
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, PROJECT_ROOT)
