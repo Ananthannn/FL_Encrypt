c6ebc3c8e078baba73a256fd5deb33733d564c71
hue9750 <rachelsara2005@gmail.com>
2026-02-02 22:34:00 +0530

    Added timing and the training history gets returned

--- model/train.py
+++ model/train.py
@@ -8,16 +8,13 @@ def train_model(x, y, model, optimizer, loss, metrics, epochs, batch_size):
     )
 
     start = time.time()
-
     history = model.fit(
         x, y,
         epochs=epochs,
         batch_size=batch_size,
         verbose=1
     )
-
     end = time.time()
+
     print(f"⏱ Training time: {end-start:.2f}s")
-
     return model.get_weights(), history.history
-
